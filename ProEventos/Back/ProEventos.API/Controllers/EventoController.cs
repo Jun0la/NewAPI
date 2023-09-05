b2f7419ab780ec613e759315bb983d8189ffac34
Jun0la <contatojjunola@gmail.com>
2023-09-05 15:58:12 -0300

    Controler e Models

--- ProEventos/Back/ProEventos.API/Controllers/EventoController.cs
+++ ProEventos/Back/ProEventos.API/Controllers/EventoController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using ProEventos.API.Models;
 
 namespace ProEventos.API.Controllers
 {
@@ -11,16 +12,52 @@ namespace ProEventos.API.Controllers
     [Route("api/[controller]")]
     public class EventoController : ControllerBase
     {
+        public IEnumerable<Evento> _evento = new Evento[]                       
+        {
+            new Evento()
+
+                {
+                    EventoId = 1, 
+                    Tema = "Angular", 
+                    Local = "Pres. Prudente", 
+                    Lote = "1°", Quantidade = 199, 
+                    DataEvento = DateTime.Now.AddDays(2).ToString(),
+                },
+
+            new Evento()
+
+                {
+                    EventoId = 2, 
+                    Tema = "Angular & .NET CORE", 
+                    Local = "Pres. Prudente", 
+                    Lote = "5°", Quantidade = 100, 
+                    DataEvento = DateTime.Now.AddDays(20).ToString(),
+                    ImageURL = "foto.png"
+                }
+        };
+
         public EventoController()
         {
 
         }
 
-        [HttpGet]
-        public string Get()
+        
+
+        [HttpGet("{id}")]
+        public IEnumerable<Evento> GetById(int id)
         {
-           return "value";
+          return _evento.Where(evento => evento.EventoId == id);
         }
+
+
+
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+          return _evento;
+        }
+
         [HttpPost]
         public string Post()
         {
