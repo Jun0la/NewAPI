4adf2936959cfa113330785c7b20f9e7e42ff76f
Jun0la <contatojjunola@gmail.com>
2023-09-12 22:02:41 -0300

    APP COMPONENT + BD.GET

--- ProEventos/Back/ProEventos.API/Startup.cs
+++ ProEventos/Back/ProEventos.API/Startup.cs
@@ -32,6 +32,7 @@ namespace ProEventos.API
             services.AddDbContext<DataContext>(context => context.UseSqlite(Configuration.GetConnectionString("Default")));
             //
             services.AddControllers();
+            services.AddCors();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Swagger", Version = "v1" });
@@ -52,6 +53,9 @@ namespace ProEventos.API
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseCors(cors => cors.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
+
 
             app.UseEndpoints(endpoints =>
             {
---
+++ ProEventos/Front/ProEventos-App/src/app/evento/evento.component.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-evento',
+  templateUrl: './evento.component.html',
+  styleUrls: ['./evento.component.scss']
+})
+export class EventoComponent implements OnInit {
+
+  public eventos: any;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {this.GetEventos();}
+
+  public GetEventos() : void { this.http.get('https://localhost:5001/api/eventos').subscribe(
+    response => this.eventos = response,
+    error => console.log(error)
+  );
+ }
+}
